16da81c05be642f6a4ba55229b8b6711a694faf8
shitakemura <shitakemura@gmail.com>
2023-05-17 13:41:33 +0900

    5-7 (add test for form validation)

--- frontend-testing-book/unittest/src/05/07/RegisterAddress.test.tsx
+++ frontend-testing-book/unittest/src/05/07/RegisterAddress.test.tsx
@@ -20,6 +20,18 @@ async function fillValuesAndSubmit() {
   return submitValues;
 }
 
+// 入力バリデーションでエラーとなるインタラクション関数
+async function fillInvalidValuesAndSubmit() {
+  const contactNumber = await inputContactNumber({
+    phoneNumber: "abc-defg-hijkl",
+    name: "田中 太郎",
+  });
+  const deliveryAddress = await inputDeliveryAddress();
+  const submitValues = { ...contactNumber, ...deliveryAddress };
+  await clickSubmit();
+  return submitValues;
+}
+
 beforeEach(() => {
   jest.resetAllMocks();
 });
@@ -30,7 +42,7 @@ test("成功時「登録しました」が表示される", async () => {
   render(<RegisterAddress />);
   const submitValues = await fillValuesAndSubmit();
   expect(mockFn).toHaveBeenCalledWith(expect.objectContaining(submitValues));
-  expect(screen.getByText("登録しました"));
+  expect(screen.getByText("登録しました")).toBeInTheDocument();
 });
 
 test("失敗時「登録に失敗しました」が表示される", async () => {
@@ -39,5 +51,21 @@ test("失敗時「登録に失敗しました」が表示される", async () =>
   render(<RegisterAddress />);
   const submitValues = await fillValuesAndSubmit();
   expect(mockFn).toHaveBeenCalledWith(expect.objectContaining(submitValues));
-  expect(screen.getByText("登録に失敗しました"));
+  expect(screen.getByText("登録に失敗しました")).toBeInTheDocument();
+});
+
+test("バリデーションエラー時「不正な入力値が含まれています」が表示される", async () => {
+  // Arrange
+  render(<RegisterAddress />);
+  // Act
+  await fillInvalidValuesAndSubmit();
+  // Assert
+  expect(screen.getByText("不正な入力値が含まれています")).toBeInTheDocument();
+});
+
+test("不明なエラー時「不明なエラーが発生しました」が表示される", async () => {
+  render(<RegisterAddress />);
+  await fillValuesAndSubmit();
+  // APIをモック化せずに実行しているのでエラーとなる
+  expect(screen.getByText("不明なエラーが発生しました")).toBeInTheDocument();
 });
